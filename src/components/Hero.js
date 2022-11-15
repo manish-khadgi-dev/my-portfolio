16df9538278af830d12431c07226a4a2c6b3efc6
Manish Khadgi <manishkhadgi123@gmail.com>
2022-11-15 16:42:05 +1100

    homepage done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from "react";
 import resume from "../assets/Resume-Manish-Khadgi.pdf";
+import { motion } from "framer-motion";
 
 export const Hero = ({}) => {
   return (
@@ -9,19 +10,35 @@ export const Hero = ({}) => {
           <div className="col-md-9  pt-5 text-info">
             Hi, i am <p />
             <span className="fw-bold text-white">
-              <h1 className="display-1">Manish Khadgi</h1>
+              <motion.h1
+                className="display-1"
+                initial={{ x: 100 }}
+                animate={{ x: 0 }}
+                transition={{ ease: "easeOut", duration: 2 }}
+              >
+                Manish Khadgi
+              </motion.h1>
             </span>
             <div className="mt-2 mb-5 fs-5 fw-bold text-white">
-              <h2>Software Engineer | App developer </h2>
+              <motion.h2
+                initial={{ x: -100 }}
+                animate={{ x: 0 }}
+                transition={{ ease: "easeOut", duration: 2 }}
+              >
+                Software Engineer | App developer{" "}
+              </motion.h2>
             </div>
             <p className="fs-8">
               I'm a Software Developer from Sydney, Australia with solid
               experience in building complex application with modern technlogy.
             </p>
             <a href={resume} download>
-              <button className="btn btn-dark text-white mt-3">
+              <motion.button
+                className="btn btn-dark text-white mt-3"
+                animate={{ x: [0, 100, 0] }}
+              >
                 Download Resume <i className="fa-solid fa-download"></i>
-              </button>
+              </motion.button>
             </a>
           </div>
         </div>
